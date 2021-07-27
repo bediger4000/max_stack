b61151167a4e638f9c9480936e3ed9ac42f70d90
Bruce Ediger <bediger8@gmail.com>
2021-07-27 14:41:09 -0600

    Exhaustive commentary

--- max1.go
+++ max1.go
@@ -7,11 +7,17 @@ import (
 	"strconv"
 )
 
+// stack nodes: linked list type. Easy to add a head node,
+// or to remove a head node. Nil value means "empty list".
 type Node struct {
 	value int
 	next  *Node
 }
 
+// struct Stack holds pointers to heads of two stacks:
+// one for data added via push() method
+// 2nd for the max data value at that level of the data stack
+// Both stacks use structs Node to hold items pushed on stack
 type Stack struct {
 	data     *Node // data values
 	maxValue *Node // max data, kept in sync with data
@@ -21,6 +27,16 @@ var (
 	NilStack = errors.New("nil Stack")
 	NoData   = errors.New("no stack data")
 )
+
+// Go can do interface types to enforce (func signature)
+// compliance with abstract types.
+type AbstractMaxStack interface {
+	max() (int, error)
+	pop() (int, error)
+	push(val int)
+}
+
+var _ AbstractMaxStack = (*Stack)(nil)
 
 func main() {
 	stack := &Stack{data: nil, maxValue: nil}
@@ -78,6 +94,7 @@ func (s *Stack) pop() (int, error) {
 		return 0, NoData
 	}
 	val := s.data.value
+	// pop both stacks in unison
 	s.data = s.data.next
 	s.maxValue = s.maxValue.next
 	return val, nil
@@ -85,14 +102,18 @@ func (s *Stack) pop() (int, error) {
 
 func (s *Stack) push(val int) {
 	if s.data == nil {
+		// empty stacks, put first item on both
 		s.data = &Node{value: val}
 		s.maxValue = &Node{value: val}
 		return
 	}
+	// careful with this part
 	max := s.maxValue.value
+	// push val on data stack
 	s.data = &Node{value: val, next: s.data}
 	if s.data.value > max {
 		max = s.data.value
 	}
+	// push max value at this level of stack on max value stack
 	s.maxValue = &Node{value: max, next: s.maxValue}
 }
